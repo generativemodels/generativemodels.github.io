d12f56403a7fffa0aeeec571c4bb05ad4028a659
Rémi Emonet <remi.emonet@reverse--com.heeere>
2025-09-13 22:43:58 +0200

    Raw import for github pages

---
+++ astro.config.mjs
@@ -0,0 +1,52 @@
+// @ts-check
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import { typst } from 'astro-typst';
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://generativemodels.github.io/',
+  //base: '/',
+  integrations: [
+    mdx(),
+    sitemap(),
+    typst({
+      options: {
+        remPx: 14,
+      },
+      target: (id) => {
+        console.debug(`Detecting ${id}`);
+        if (id.endsWith(".html.typ") || id.includes("/html/")) return "html";
+        return "svg";
+      },
+      // === <img src="xxx.svg"> instead of inlined <svg> ===
+      emitSvg: true,
+      // emitSvgDir: ".astro/typst"
+      // === Add non-system fonts here ===
+      // fontArgs: [
+      //   { fontPaths: ['/system/fonts', '/user/fonts'] },
+      //   { fontBlobs: [customFontBuffer] }
+      // ],
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [
+        rehypeKatex,
+        {
+          macros: {},
+          trust: true,
+          strict: false,
+        },
+      ],
+    ],
+  },
+  vite: { ssr: { external: ["@myriaddreamin/typst-ts-node-compiler"] } },
+});
